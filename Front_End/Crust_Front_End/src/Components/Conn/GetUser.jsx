369e64cecae848cc4c16654ba643794145947f3c
Mysterious-Cart <soengchhan@gmail.com>
2024-11-21 20:03:30 +0700

    Adding more UI Element

--- Front_End/Crust_Front_End/src/Components/Conn/GetUser.jsx
+++ Front_End/Crust_Front_End/src/Components/Conn/GetUser.jsx
@@ -3,7 +3,7 @@ import { useLazyQuery, gql } from "@apollo/client";
 import { Load_User } from "../../Graphql/Queries";
 
 function GetUser({result}){
-
+    
     const [LoadUser,{error, loading, data}] = useLazyQuery(Load_User)
     if(error){console.log(error); return}
 
--- Front_End/Crust_Front_End/src/Components/UserProfileList.jsx
+++ Front_End/Crust_Front_End/src/Components/UserProfileList.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import GetUser from "./Conn/GetUser";
-import { Card, Stack, TextField, Paper, ListItem, Grid, List, ListItemText } from "@mui/material";
+import { Card, Stack, TextField, Paper, ListItem, Grid, List, ListItemText, Typography, Avatar, Grid2, Skeleton } from "@mui/material";
 
 function UserProfileList(width, height){
 
@@ -24,16 +24,28 @@ function UserProfileList(width, height){
         <GetUser result={StoreRecieveUser}/>
         <div style={{heigh:{height}, minHeight:{height}, width:width, backgroundColor:"transparent"}}>
             <Stack style={{padding:"10px"}}>
-                <TextField/>
+                <TextField size="small" placeholder="Search..."/>
+                <Typography variant="p" style={{marginTop:"10px", marginLeft:"5px", height:"10px",color:"grayText"}}>Direct Message</Typography>
             </Stack>
             <List>
                 {
                     user.map((EachUser,index) => (
                         <ListItem key={index}>
-                            <ListItemText primary={EachUser.name} secondary="loading.."/>
+                            <Grid2 container columnGap={1}>
+                                <Grid2 size={5} alignContent={"center"}>
+                                    <Avatar/>
+                                </Grid2>
+                                <Grid2 size={6}>
+                                    <ListItemText primary={EachUser.name} secondary={"loading.."}>
+                                        
+                                    </ListItemText>
+                                </Grid2>
+                                
+                            </Grid2>
                         </ListItem>
                     ))
                 }
+                
             </List>
         </div>
     </div>
